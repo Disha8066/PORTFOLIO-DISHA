647d696b4112ac107f222a87f96497f3b2098835
Disha <dishamahia04@gmail.com>
2026-07-17 13:01:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+navLinks.forEach(link => {
+        link.classList.remove("active");
+if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+const text = "Student | | Web Developer";
+const typing = document.querySelector(".hero p");
+let index = 0;
+function typeText() {
+    if (index < text.length) {
+        typing.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeText, 80);
+    }}
+typing.textContent = "";
+typeText();
+const cards = document.querySelectorAll(".card, .skills div");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, {
+    threshold: 0.2
+});
+cards.forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.8s";
+    observer.observe(card);
+});
